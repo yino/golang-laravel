2446138d374447883206321bf6125474deae3cb9
sun17 <sunxiaohu@vchangyi.com>
2020-11-11 10:13:43 +0800

    run

---
+++ extend/autoload/run.go
@@ -0,0 +1,47 @@
+package autoload
+
+import (
+	"fmt"
+	"gin-api/app"
+	"gin-api/app/models"
+	"gin-api/app/validate"
+	"gin-api/extend/log"
+	"gin-api/extend/redis"
+)
+
+// 加载log
+func loadLog() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutPutDir(app.GetLogDir())
+}
+
+// 自动加载
+func Run() {
+	// 加载log
+	loadLog()
+	// 中文验证器
+	verifyZn()
+	// mysql pool
+	mysqlInitPool()
+	// redis pool
+	redisInitPool()
+}
+
+// 加载中文验证器
+
+func verifyZn() {
+	// 检测是否支持中文扩展 （用于验证器返回中文）
+	if err := validate.InitTrans("zh"); err != nil {
+		fmt.Printf("init trans failed, err:%v\n", err)
+		return
+	}
+}
+
+func mysqlInitPool()  {
+	// 创建MySql连接池
+	models.InitDbConnection()
+}
+func redisInitPool()  {
+	// 创建redis 连接池
+	redis.InitConnectionPool()
+}
--- main.go
+++ main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"gin-api/extend/autoload"
 	"gin-api/extend/redis"
 	"github.com/gin-gonic/gin"
 
-	"gin-api/app"
 	"gin-api/app/exception"
 	"gin-api/app/models"
-	"gin-api/app/validate"
 	"gin-api/config"
-	"gin-api/extend/log"
 	"gin-api/routes"
 )
 
 func main() {
-
-
-	// log 模块
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	log.SetOutPutDir(app.GetLogDir())
-	// 检测是否支持中文扩展 （用于验证器返回中文）
-	if err := validate.InitTrans("zh"); err != nil {
-		fmt.Printf("init trans failed, err:%v\n", err)
-		return
-	}
-
-	// 创建MySql连接池
-	models.InitDbConnection()
-	// 创建redis 连接池
-	redis.InitConnectionPool()
-
+	// 自动加载
+	autoload.Run()
 	// 关闭连接
 	defer redis.Connection().Close()
 	defer models.Db().Close()
